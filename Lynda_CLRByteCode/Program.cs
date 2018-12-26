d10a47ca78121e288cf82c5744007d00a11430da
Sean Newman <seangnewman@msn.com>
2018-12-26 13:30:44 -0700

    Add project files.

---
+++ Lynda_CLRByteCode/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lynda_CLRByteCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
