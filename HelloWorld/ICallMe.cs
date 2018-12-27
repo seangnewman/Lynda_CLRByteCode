692fdada32b076175f0056b8a4ee1e7b6fff826e
Sean Newman <seangnewman@msn.com>
2018-12-27 06:13:52 -0700

    Initial commit

---
+++ HelloWorld/ICallMe.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    interface ICallMe
+    {
+         string Name { get;  }
+        void CallMe();
+    }
+}
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            var rand = new Random();
+
+            if(rand.Next() % 2 == 0)
+            {
+                Console.WriteLine("It is even");
+            }
+            else
+            {
+                Console.WriteLine("It is odd");
+            }
+        }
+    }
+}
